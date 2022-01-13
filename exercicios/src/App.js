40087c5567e66c37c3f78e5097fa5d60150270f7
bruunooAlmeida <almeidabrunoti@gmail.com>
2022-01-13 20:46:06 -0300

    1 Commit

---
+++ exercicios/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { View } from 'react-native'
+
+import { Comp1 , Comp2 }  from './components/Multi'
+import Primeiro from "./components/Primeiro";
+
+export default () => (
+    < View>
+
+        <Comp1 />
+        <Comp2 />
+        <
+    </View>
+
+) 
+
+    
+
+
---
+++ exercicios/src/components/Multi.js
@@ -0,0 +1,15 @@
+import  React   from 'react'
+import  { Text } from 'react-native'
+
+function Comp() {
+    return <Text> Comp #1 Oficial</Text>
+}
+
+export function Comp1() {
+    return <Text> Comp #1</Text>
+}
+
+
+export function Comp2() {
+    return <Text> Comp #2</Text>
+}
